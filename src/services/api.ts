633eafea7f2c34f3f1f28bd801705a6d3569be0c
Muller Esposito <mulleresposito@hotmail.com>
2021-07-28 07:49:21 -0300

    refactor(api): update baseUrl;

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,7 @@ import axios from 'axios';
 import "dotenv/config";
 
 const baseURL = process.env.NODE_ENV === "production" 
-  ? "http://18.118.36.3:3333/api"
+  ? "http://18.118.36.3/api"
   : "http://localhost:3333/api";
 
-console.log(process.env.NODE_ENV);
 export const api = axios.create({ baseURL });
